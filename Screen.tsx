f1ce3d8f38c856681a622b77faf78a4de5c08adc
Laurens Lamberts <laurens.lamberts@weareyou.com>
2022-05-04 19:30:05 +0200

    removed reanimated, added react-navigation

---
+++ Screen.tsx
@@ -0,0 +1,93 @@
+import React, {useEffect, useState} from 'react';
+import {Canvas, RoundedRect, Shadow} from '@shopify/react-native-skia';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+const NUMBER_OF_ITEMS = 100;
+
+const getRandomColor = () => {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+};
+
+const Screen = () => {
+  const [items, setItems] = useState([]);
+  const [show, setShow] = useState(true);
+  const [randomColor, setRandomColor] = useState(getRandomColor());
+
+  const onPress = () => {
+    setShow(_show => !show);
+    setRandomColor(getRandomColor());
+  };
+
+  useEffect(() => {
+    setItems(new Array(NUMBER_OF_ITEMS).fill(0));
+  }, []);
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <TouchableOpacity
+        style={{
+          backgroundColor: randomColor,
+          alignItems: 'center',
+          marginHorizontal: 20,
+          paddingVertical: 20,
+        }}
+        activeOpacity={0.8}
+        onPress={onPress}>
+        <Text style={{color: 'white'}}>{show ? 'Hide' : 'Show'}</Text>
+      </TouchableOpacity>
+      {show && (
+        <View style={styles.container}>
+          <View style={styles.mode}>
+            {items.map((item, index) => (
+              <View
+                key={index}
+                style={{
+                  margin: 2,
+                }}>
+                <Canvas style={{width: 30, height: 40}}>
+                  <RoundedRect
+                    x={0}
+                    y={0}
+                    width={30}
+                    height={40}
+                    r={8}
+                    color={randomColor}>
+                    <Shadow
+                      dx={0}
+                      dy={10}
+                      blur={10}
+                      color={'rgba(11,34,46,0.20)'}
+                    />
+                  </RoundedRect>
+                </Canvas>
+              </View>
+            ))}
+          </View>
+        </View>
+      )}
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  mode: {
+    paddingHorizontal: 10,
+    paddingVertical: 4,
+    margin: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap',
+  },
+});
+
+export default Screen;
