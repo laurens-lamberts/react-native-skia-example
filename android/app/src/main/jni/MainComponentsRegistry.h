3e1ed60352ecb7fc6aadb638ce11a7e99e9482e9
Laurens Lamberts <laurens.lamberts@weareyou.com>
2022-05-04 19:40:33 +0200

    completed rename

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/skiareanimatedcrash/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/skiaexample/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
--- ios/SkiaExample/AppDelegate.mm
+++ ios/SkiaExample/AppDelegate.mm
@@ -41,7 +41,7 @@
   bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
 #endif
 
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"SkiaReanimatedCrash", nil);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"SkiaExample", nil);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
