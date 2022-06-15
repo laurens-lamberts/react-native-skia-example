94dc1c369bf6c93c58116cbcb0c6d6cea7147ace
Laurens Lamberts <laurens.lamberts@weareyou.com>
2022-06-15 23:33:43 +0200

    slowed-down the ball

--- src/examples/Labyrinth/Config.tsx
+++ src/examples/Labyrinth/Config.tsx
@@ -3,5 +3,5 @@ export const HOLE_SIZE = 44;
 export const WALL_WIDTH = 20;
 
 // Physics
-export const BALL_SPEED_FACTOR = 0.4;
+export const BALL_SPEED_FACTOR = 0.1;
 export const FALL_SENSITIVITY = 0; // larger numbers makes the ball fall easier into the hole
