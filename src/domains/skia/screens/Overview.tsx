8e2c3e093a16b9ad14f95dcf404a6e4eb8f78f81
Laurens <laurenslamberts@hotmail.com>
2024-10-08 23:51:04 +0200

    refactoring, turned to flashlist

--- src/domains/skia/screens/Overview.tsx
+++ src/domains/skia/screens/Overview.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { ScrollView, Text, TouchableOpacity, View } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { Text, TouchableOpacity, View } from "react-native";
+import {
+  NavigationContainerRef,
+  useNavigation,
+} from "@react-navigation/native";
 import {
   Compass,
   Fire,
@@ -16,77 +19,68 @@ import {
 } from "phosphor-react-native";
 import useTheme from "@app/hooks/useTheme";
 import { SkiaRoutes } from "../navigation/Stack";
+import { FlashList } from "@shopify/flash-list";
 
-const Icon = ({ routeName }: { routeName: keyof SkiaRoutes }) => {
-  switch (routeName) {
-    case "Overview":
-      return <Fire />;
-    case "Floating Balls":
-      return <Balloon />;
-    case "Flappy":
-      return <Bird />;
-    case "Maze":
-      return <BowlingBall />;
-    case "Compass":
-      return <Compass />;
-    case "Fire":
-      return <Fire />;
-    case "Springboard":
-      return <AppleLogo />;
-    case "Mount":
-      return <Blueprint />;
-    case "Blur":
-      return <Drop />;
-    case "Resize":
-      return <Resize />;
-    case "Maps":
-      return <MapTrifold />;
-    default:
-      return <Question />;
-  }
+const iconMap: { [key in keyof SkiaRoutes]?: JSX.Element } = {
+  Overview: <Fire />,
+  "Floating Balls": <Balloon />,
+  Flappy: <Bird />,
+  Maze: <BowlingBall />,
+  Compass: <Compass />,
+  Fire: <Fire />,
+  Springboard: <AppleLogo />,
+  Mount: <Blueprint />,
+  Blur: <Drop />,
+  Resize: <Resize />,
+  Maps: <MapTrifold />,
 };
 
+const Icon = ({ routeName }: { routeName: keyof SkiaRoutes }) =>
+  iconMap[routeName] || <Question />;
+
 const SkiaOverview = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation<NavigationContainerRef<SkiaRoutes>>();
   const theme = useTheme();
 
-  const [routeNames, setRouteNames] = useState<string[]>([]);
+  const [routeNames, setRouteNames] = useState<(keyof SkiaRoutes)[]>([]);
 
   useEffect(() => {
-    const routeNames = [...navigation.getState().routeNames];
+    const routeNames = [
+      ...navigation.getState().routeNames,
+    ] as (keyof SkiaRoutes)[];
     routeNames.shift();
     setRouteNames(routeNames);
   }, []);
 
   return (
     <View style={{ flex: 1, backgroundColor: "#FFF" }}>
-      <ScrollView>
-        {routeNames.map((routeName, index) => {
-          return (
-            <TouchableOpacity
-              key={index}
-              onPress={() => navigation.navigate(routeName)}
+      <FlashList
+        data={routeNames}
+        renderItem={({ item: routeName }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate(routeName)}
+            style={{
+              padding: 12,
+              backgroundColor: "#FFF",
+              borderColor: "#CCC",
+              borderBottomWidth: 1,
+            }}
+          >
+            <View
               style={{
-                padding: 12,
-                backgroundColor: "#FFF",
-                borderColor: "#CCC",
-                borderBottomWidth: 1,
+                flexDirection: "row",
+                alignItems: "center",
+                gap: theme.space.m,
               }}
             >
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  gap: theme.space.m,
-                }}
-              >
-                <Icon routeName={routeName} />
-                <Text>{routeName}</Text>
-              </View>
-            </TouchableOpacity>
-          );
-        })}
-      </ScrollView>
+              <Icon routeName={routeName} />
+              <Text>{routeName}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(_item, index) => index.toString()}
+        estimatedItemSize={48}
+      />
     </View>
   );
 };
